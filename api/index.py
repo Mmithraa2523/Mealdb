1edad4279b2cd0982f39bb717697e9f9007f838b
Mithraa J <mithraa2325@gmail.com>
2026-01-05 21:36:25 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+MEALDB_BASE_URL = "https://www.themealdb.com/api/json/v1/1"
+
 @app.route("/")
 def home():
-    return "MealDB Flask is running on Mithraa Vercel 🚀"
+    return render_template("search.html")
+
+@app.route("/search")
+def search():
+    q = request.args.get("q", "")
+    meals = []
+
+    if q:
+        data = requests.get(f"{MEALDB_BASE_URL}/search.php?s={q}").json()
+        meals = data.get("meals") or []
+
+    return render_template("meals.html", meals=meals)
 
 app = app
-
