a91ea350c477abcceaf7026806030eea0d23466a
Mithraa J <mithraa2325@gmail.com>
2026-01-05 21:43:37 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 
 MEALDB_BASE_URL = "https://www.themealdb.com/api/json/v1/1"
 
+import os
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.environ["MYSQLHOST"],
+        user=os.environ["MYSQLUSER"],
+        password=os.environ["MYSQLPASSWORD"],
+        database=os.environ["MYSQLDATABASE"],
+        port=int(os.environ["MYSQLPORT"])
+    )
 @app.route("/")
 def home():
     return render_template("search.html")
@@ -21,3 +32,4 @@ def search():
     return render_template("meals.html", meals=meals)
 
 app = app
+
